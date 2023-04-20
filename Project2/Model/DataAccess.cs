c5041a8d250cadc10a722db3a2b3861892b5f787
Positron234 <andreigaivoronskikh120@gmail.com>
2023-04-20 16:46:16 +0700

    Добавьте файлы проекта.

---
+++ Project2/Model/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using Project.Model;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace ProjectBilling.DataAccess
+{
+    public interface IDataService
+    {
+        IList<Product> Product();
+    }
+
+    public class DataServiceStub : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataServiceStub()
+        {
+            Database.EnsureCreated();
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(DESKTOP-C8L1TCD)\\mssqllocaldb;Database=SrokProduct;Trusted_Connection=True");
+        }
+    }
+}
---
+++ Project2/Model/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public interface IProduct
+    {
+         string Name { get; set; }
+         string DateStart { get; set; }
+         string Srok { get; set; }
+        void Update(IProduct product);
+    }
+    public class Product: IProduct
+    {
+        public string Name { get; set; }
+        public string DateStart { get; set; }
+        public string Srok { get; set; }
+        public void Update(IProduct product)
+        {
+            Name = product.Name;
+            DateStart = product.DateStart;
+            Srok = product.Srok;
+        }
+    }
+    
+}
